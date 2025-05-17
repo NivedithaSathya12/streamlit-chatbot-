d69f23c5700a88f75b57ff78a79616cdea0d4f52
NivedithaSathya12 <niveditha.sathyanarayana@gmail.com>
2025-05-17 15:20:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import os
+os.environ["GOOGLE_API_KEY"]=st.secrets["GOOGLE_API_KEY"]
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
